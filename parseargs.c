e01e8f58867e68e6a41efb565177d0668b49710e
kli19 <kli19@stuy.edu>
2018-11-14 15:30:44 -0500

    classwork

--- parseargs.c
+++ parseargs.c
@@ -1,18 +1,36 @@
 #include<stdio.h> 
 #include<stdlib.h> 
-#include<unistd.h> 
-int main() 
-{ 
-        //A null terminated array of character  
-        //pointers 
-        char *args[]={"./ab",NULL}; 
-        execlp(args[0],
-	       "./ab",args[1]); 
+#include<unistd.h>
+#include<string.h>
+
+void parse_args( char * line ) {
+  char *s1 = line;
+  while(s1){
+    printf("[%s]\n", strsep(&s1, " "));
+    //printf("[%s]\n", s1);
+  }
+}
+
+int main(){
+  char line[100]="hi testing kil";
+  //scanf("%s", line);
+  parse_args(line);
+ 
+
+
+
+
+  
+  /* //A null terminated array of character   */
+  /* //pointers  */
+  /* char *args[]={"./ab",NULL};  */
+  /* execlp(args[0], */
+  /* 	 "./ab",args[1]);  */
       
-        /*All statements are ignored after execvp() call as this whole  
-        process(execDemo.c) is replaced by another process (EXEC.c) 
-        */
-        printf("Ending-----\n"); 
+  /* /\*All statements are ignored after execvp() call as this whole   */
+  /*   process(execDemo.c) is replaced by another process (EXEC.c)  */
+  /* *\/ */
+  /* printf("Ending-----\n");  */
       
-    return 0; 
+  return 0; 
 } 
