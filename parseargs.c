d5ad595bd9c857fc364419d47b90384b85d43602
kli19 <kli19@stuy.edu>
2018-11-14 15:36:19 -0500

    classwork

--- parseargs.c
+++ parseargs.c
@@ -3,13 +3,26 @@
 #include<unistd.h>
 #include<string.h>
 
-void parse_args( char * line ) {
+void parse_argsBETA( char * line ) {
   char *s1 = line;
   while(s1){
     printf("[%s]\n", strsep(&s1, " "));
     //printf("[%s]\n", s1);
   }
 }
+char ** parse_args( char * line ) {
+  char *s1 = line;
+  char stuff[5][100];
+  int i =0;
+  while(s1){
+    stuff[i] = strsep(&s1, " ");
+    printf("%s", stuff[i]);
+    i++;
+    //printf("[%s]\n", strsep(&s1, " "));
+    //printf("[%s]\n", s1);
+  }
+}
+
 
 int main(){
   char line[100]="hi testing kil";
