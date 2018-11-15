66333f110447fe3e9574f270e6937b6c6be791ae
michaellikaiye <mye1@stuy.edu>
2018-11-15 15:05:43 -0500

    hi

--- parseargs.c
+++ parseargs.c
@@ -4,14 +4,12 @@
 #include<string.h>
 
 
-char ** parse_args( char * line ) {
-  char ** arr = malloc(10 * sizeof(char *));
-  char *s1 = line;
+char ** parse_args( char * line) {
+  char ** arr = malloc(5 * sizeof(char *));
 
   int i = 0;
-  while(s1){
-    arr[i] = strsep(&s1, " ");
-    //printf("%s\n", arr[i]);
+  while(line){
+    arr[i] = strsep(&line, " ");
     i++;
   }
 
